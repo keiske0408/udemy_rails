6c53bef5f1760f646109906cdb3243635bd8bee6
keiske0408 <kieske0408@gmail.com>
2024-10-09 13:53:12 +0800

    add new articles page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,14 @@ class ArticlesController < ApplicationController
     #byebug
     @article = Article.find(params[:id])
   end
-
   def index
     @articles = Article.all
   end
+  def new
+
+  end
+  def create
+    render plain: params[:article]
+  end
+
 end

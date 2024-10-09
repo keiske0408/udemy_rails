39fac061b7599b9f133f29161da3c48980a4df4e
keiske0408 <kieske0408@gmail.com>
2024-10-09 15:54:44 +0800

    add save newly created articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   def new
-
+    @article = Article.new
   end
   def create
-    render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to articles_path
   end
 
+#   private
+#   def article_params
+#     params.require(:article).permit(:title, :description)
+#   end
 end

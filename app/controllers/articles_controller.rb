51a5a7f1abb245cb535a92d7df5774afd1e99549
keiske0408 <kieske0408@gmail.com>
2024-10-09 16:47:48 +0800

    modify: flash messages and errors on new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,25 @@
 class ArticlesController < ApplicationController
   def show
-    #byebug
+    # byebug
     @article = Article.find(params[:id])
   end
+
   def index
     @articles = Article.all
   end
+
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to articles_path
+    if @article.save
+      flash[:notice] = "Article successfully created"
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
-#   private
-#   def article_params
-#     params.require(:article).permit(:title, :description)
-#   end
 end

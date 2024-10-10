ce31f91363a4cec166d6657a8eec05628b219306
keiske0408 <kieske0408@gmail.com>
2024-10-10 12:33:11 +0800

    add: update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,4 +22,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description))
+      flash[:notice] = "Article edited successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 end

5ff1c07040c0bc011c18584a4a798b27b7f6680e
keiske0408 <kieske0408@gmail.com>
2024-10-15 12:40:01 +0800

    Add: user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User successfully logged in, Welcome #{@user.username} to the Alpha Blog!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
